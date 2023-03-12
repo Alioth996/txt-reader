62b59c7a6616f098e72adefae168998c5dd84e06
alex <2844520415@qq.com>
2023-03-12 18:42:20 +0800

    :bug: home页面移动端适配

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,27 +1,27 @@
 <template>
-    <div id="home" p="5">
-        <div class="upload-header">
+    <div id="home" h-full>
+        <div class="upload-header" p-3>
             <input type="file" name="txt-file" accept=".txt" @change='uploadBook' />
         </div>
 
         <div class="book-history">
-            <ul class="novel-list">
-                <li class="novel" v-for="book in bookList" v-show="book.bookID" p="5" flex gap-5>
-                    <div class="book-cover" max-w-sm py-15 px-xs bg-light b-rd-1 text-white text-sm font-600 text-center>
+            <ul class="novel-list" px-sm>
+                <li class="novel" v-for="book in bookList" v-show="book.bookID" py-sm flex gap-5 w-full sm-justify-start
+                    justify-between>
+                    <div class="book-cover" py-13 px-sm bg-light b-rd-1 text-white text-sm font-600 text-center>
                         {{ book.name }}
                     </div>
-                    <div class="book-meta" text-sm flex="~ col" justify-center>
-                        <p v-show="book.name">书名: {{ book.name }}</p>
-                        <p py-1 v-show="book.author">作者: {{ book.author }}</p>
-                        <p py-1 v-show="book.uploadTime">更新时间: {{ book.uploadTime }}</p>
-                        <p v-show="book.size">大小: {{ book.size }} MB</p>
-                        <!-- <p v-show="book.bookID">id: {{ book.bookID }}</p> -->
+                    <div class="book-meta" min-w-35 text-sm flex="~ col" justify-center gap-1>
+                        <p v-show="book.name" class="single-line" text-dark font-600 text-md> {{ book.name }}</p>
+                        <p py-1 v-show="book.author" text-light>{{ book.author }}</p>
+                        <p py-1 v-show="book.uploadTime" text-light>{{ book.uploadTime }}</p>
+                        <p v-show="book.size" text-light>{{ book.size }} MB</p>
                     </div>
 
                     <div flex-c>
                         <button text-md px-sm py-xs b-rd-1 bg-active text-white text-sm font-600 cursor="pointer"
-                            hover="opacity-90 text-bookbg">阅
-                            读</button>
+                            hover="opacity-90 text-bookbg">阅 读
+                        </button>
                     </div>
                 </li>
             </ul>
@@ -46,7 +46,8 @@ const { uploadBook, bookList } = useBook()
     border-bottom: 1px solid #ccc;
 
     .book-cover {
-        width: 110px;
+        min-width: 100px;
+        width: 100px;
         box-shadow: 0px 0px 5px 2px #00000050;
         letter-spacing: .2rem;
         text-overflow: ellipsis;
@@ -57,8 +58,7 @@ const { uploadBook, bookList } = useBook()
     }
 }
 
-
-.singe-line {
+.single-line {
     text-overflow: ellipsis;
     overflow: hidden;
     word-break: break-all;
