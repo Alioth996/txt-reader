d5de4696fa4f2eb6a1c5309d659af4d314a311e8
alex <2844520415@qq.com>
2023-03-12 19:58:53 +0800

    :lipstick: 修改部分样式

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,8 +2,7 @@
     <div id="home">
         <div class="upload-header">
             <input class="file" type="file" name="txt-file" accept=".txt" @change='uploadBook'>
-            <p class="upload-icon">+</p>
-            <p class="upload-text">上传txt文件</p>
+            <p class="upload-icon" p-y-3>+</p>
         </div>
         <div class="book-history">
             <ul class="novel-list" px-sm>
@@ -32,20 +31,23 @@
 </template>
 
 <script setup lang='ts'>
-    import { useBook } from '@/hooks/useBook'
-    const { uploadBook, bookList } = useBook()
+import { useBook } from '@/hooks/useBook'
+const { uploadBook, bookList } = useBook()
 </script>
 
 <style scoped lang="less">
-#home{
+#home {
     width: 60%;
     margin: 0 auto;
     padding: 0 10px;
+    min-height: 100vh;
+
     // portrait 竖屏
     @media screen and (orientation: portrait) {
         width: 100%;
     }
-    .upload-header{
+
+    .upload-header {
         position: relative;
         left: 50%;
         transform: translateX(-50%);
@@ -61,6 +63,7 @@
         line-height: 20px;
         cursor: pointer;
         text-align: center;
+
         input {
             position: absolute;
             font-size: 100px;
@@ -69,22 +72,27 @@
             opacity: 0;
             cursor: pointer;
         }
+
         &:hover {
             background: #2b333e;
             text-decoration: none;
             cursor: pointer;
         }
-        .upload-icon{
+
+        .upload-icon {
             font-size: 50px;
         }
-        .upload-text{
+
+        .upload-text {
             padding-top: 20px;
         }
     }
+
     .novel {
-    border-bottom: 1px solid #ccc;
+        border-bottom: 1px solid #ccc;
     }
 }
+
 .novel {
     border-bottom: 1px solid #ccc;
 
