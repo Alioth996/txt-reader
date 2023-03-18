231045b081ff88db6c313d481a23cc1f4fc57b06
Alioth <2844520415@qq.com>
2023-03-18 23:56:51 +0800

    💄style: 使用 navieui

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <RouterView />
+  <n-dialog-provider>
+    <RouterView />
+  </n-dialog-provider>
 </template>
+
+<script setup lang="ts">
+
+import { NDialogProvider } from 'naive-ui'
+</script>
